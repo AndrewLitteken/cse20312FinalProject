cf3a919c91917bd7f0f319d19d41cfc7a593c728
Andrew Litteken <andrew.litteken@gmail.com>
2017-04-16 19:59:22 -0400

    Updated to be able to show all the songs in a persons library

--- dummy/testapp/authex.py
+++ dummy/testapp/authex.py
@@ -15,9 +15,15 @@ token = util.prompt_for_user_token(username, scope)
 
 if token:
 	sp = spotipy.Spotify(auth=token)
-	results = sp.current_user_saved_tracks()
-	for item in results['items']:
-		track = item['track']
-		print track['name'] + ' - ' + track['artists'][0]['name']
+	offset = 0
+	count = 50
+	while(count==50):
+		count = 0
+		results = sp.current_user_saved_tracks(50,offset)
+		for item in results['items']:
+			count+=1
+			track = item['track']
+			print track['name'] + ' - ' + track['artists'][0]['name']
+		offset+=50
 else:
 	print "Can't get token for", username
